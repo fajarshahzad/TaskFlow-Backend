d7fbab296166d1957d2f910ef776ae866f4f2a0d
fajarshahzad <ahsanshahzad112345@gmail.com>
2026-01-06 10:42:15 +0500

    fix

--- server.js
+++ server.js
@@ -19,8 +19,13 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
+
